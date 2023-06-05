6ad7b8fa1286bb6cbb4efb55876f13b8e8af0a8b
M4stersMasterm <mo.mahdi1379@gmail.com>
2023-06-05 14:59:29 +0330

    User model added and registered in the user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,45 @@
+"""
+Tests for models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful"""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password,
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new user"""
+        sample_emails = [
+            ['test1@EXAMPLE.com','test1@example.com'],
+            ['Test2@Example.com','Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample')
+            self.assertEqual(user.email,expected)
+
+    def test_new_user_without_email_raise_error(self):
+        """Test that a user cant create account without providing email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('','test123')
+
+    def test_create_super_user(self):
+        """Test creating SU"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
